ab984e2252db01cb6291e06253ede9dd2a655a66
Yuta Iwama <ganmacs@gmail.com>
2019-09-13 19:21:24 +0900

    set 0

--- ext/chunkio/chunkio.h
+++ ext/chunkio/chunkio.h
@@ -20,30 +20,21 @@ static const rb_data_type_t chunkio_context_type =
     {
      "ChunkIO::Context",
      {0, (void (*)(void *))chunkio_context_free, 0,},
-     0, 0,
-#ifdef RUBY_TYPED_FREE_IMMEDIATELY
-     RUBY_TYPED_FREE_IMMEDIATELY,
-#endif
+     0, 0, 0
     };
 
 static const rb_data_type_t chunkio_stream_type =
     {
      "ChunkIO::Stream",
      {0, (void (*)(void *))chunkio_stream_free, 0,},
-     0, 0,
-#ifdef RUBY_TYPED_FREE_IMMEDIATELY
-     RUBY_TYPED_FREE_IMMEDIATELY,
-#endif
+     0, 0, 0
     };
 
 static const rb_data_type_t chunkio_chunk_type =
     {
      "ChunkIO::Chunk",
      {0, (void (*)(void *))chunkio_chunk_free, 0,},                /* TODO free ch->name and ch->head */
-     0, 0,
-#ifdef RUBY_TYPED_FREE_IMMEDIATELY
-     RUBY_TYPED_FREE_IMMEDIATELY,
-#endif
+     0, 0, 0
     };
 
 struct cio_ctx *UnwrapChunkIOContext(VALUE self);
